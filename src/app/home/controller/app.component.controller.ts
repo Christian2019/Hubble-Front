779f4ab9c78958c3ea6a1160fffee8ade7b64f3e
Guilherme Munaretto <munaretto.guilherme@gmail.com>
2019-04-12 20:17:51 -0300

    Sanitização da estrutura

--- src/app/home/controller/app.component.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-home',
-    templateUrl: './app.component.controller.html',
-    styleUrls: ['./app.component.controller.scss']
-})
-export class AppComponentController {}
