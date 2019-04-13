1ee3d57ed3532ed69feb8c63084ab32e1fe396ee
Guilherme Munaretto <guilherme.munaretto@acad.pucrs.br>
2019-04-13 02:12:49 -0300

    Ajuste na exibição do icone de menu sanduiche quando em telas desktop

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -10,11 +10,21 @@ import { map } from 'rxjs/operators';
 })
 export class SidenavComponent {
 
+  sidenavOpened: boolean = true;
+  @ViewChild('drawer') drawer;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  /**
+   * Opens/closes the sidenav
+   */
+  menuToggle() {
+    this.drawer.toggle();
+    this.sidenavOpened = !this.sidenavOpened;
+  }
 
 }
