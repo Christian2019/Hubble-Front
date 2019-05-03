317340d1495ba7650ae0b356b0103fe9036e3db9
Guilherme Munaretto <munaretto.guilherme@gmail.com>
2019-05-03 04:49:51 -0300

    Criada a primeira versão do service para postar no banco; Adicionado proxy para corrigir problemas de bloqueio de CORS; Validada a entrada de datas de inicio e fim dos eventos

--- src/app/pages/create-event/controller/create-event.controller.component.ts
+++ src/app/pages/create-event/controller/create-event.controller.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from 'selenium-webdriver/http';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { EventsService } from 'src/app/shared/services/events.service';
+import { FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-create-event',
@@ -9,12 +12,27 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class CreateEventControllerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventService: EventsService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+
   }
 
-  onFormSubmitted(){
+
+  onFormSubmitted($event: FormGroup[]){
+    const response = this.eventService.create($event)
+      .subscribe(
+        (success: HttpResponse<JSON>) => {
+          this.snackBar.open('Evento cadastrado com sucesso!', 'Ok', {duration: 5000})
+        },
+        (error: HttpErrorResponse) => {
+          this.snackBar.open('Não foi possível cadastrar o evento. (Erro: '+error.message+')', 'Ok', {duration: 5000})
+        }
+      )
+
+
+
+
 
   }
 }
--- src/app/shared/components/sidenav/presenter/sidenav.component.ts
+++ src/app/shared/components/sidenav/presenter/sidenav.component.ts
@@ -10,22 +10,29 @@ import { map } from 'rxjs/operators';
 })
 export class SidenavComponent {
 
-  sidenavOpened: boolean = true;
   @ViewChild('drawer') drawer;
+  @Input() opened: boolean;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
     map(result => result.matches)
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  isTablet$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Tablet)
+  .pipe(
+    map(result => result.matches)
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+
+  }
 
   /**
    * Opens/closes the sidenav
    */
   menuToggle() {
     this.drawer.toggle();
-    this.sidenavOpened = !this.sidenavOpened;
   }
 
 }
