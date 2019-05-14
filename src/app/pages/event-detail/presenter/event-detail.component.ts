036b82756971ca92673b370e55b369cf94976c67
Luiz <luizpedrofg@gmail.com>
2019-05-13 22:04:54 -0300

    criei o esqueleto do site do event detail

--- src/app/pages/event-detail/presenter/event-detail.component.ts
+++ src/app/pages/event-detail/presenter/event-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EventsService } from 'src/app/shared/services/events.service';
 import { Event } from './EventJson/event';
+import { async } from 'q';
 
 @Component({
   selector: 'app-event-detail',
@@ -12,12 +13,12 @@ export class EventDetailComponent implements OnInit {
   //@Input() eventDetail: EventDetailComponent;
   varr = "5cc319e02f47619a17089bd5";
   jsonInf: Event;
+  // date: Date;
 
   constructor(private eventService: EventsService) { }
 
   ngOnInit() {
-    this.eventService.getById(this.varr).subscribe((data: any) => this.jsonInf = data);
-    
+    this.eventService.getById(this.varr).subscribe((data: any) => this.jsonInf = data); 
   }
 
 }
