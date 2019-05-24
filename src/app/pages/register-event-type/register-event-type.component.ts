8c9778239bad3ca5eaa19e4d3ffed4faee742b5e
Gustavo Jesinski Batista <gustavojesinskib@gmail.com>
2019-05-24 20:18:21 -0300

    code cleaning

--- src/app/pages/register-event-type/register-event-type.component.ts
+++ src/app/pages/register-event-type/register-event-type.component.ts
@@ -1,42 +1,5 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
 
-// export interface tabCategorias {
-//   name: string;
-// }
-
-// const CATEGORIES_DATA: tabCategorias[] = [
-//   {name: 'Teste'},
-//   {name: 'teste2'},
-// ];
-
-
-// @Component({
-//   selector: 'app-register-event-type',
-//   templateUrl: './register-event-type.component.html',
-//   styleUrls: ['./register-event-type.component.scss']
-// })
-
-
-
-
-// export class RegisterEventTypeComponent implements OnInit {
-//   eventTypeForm: FormGroup;
-  
-//   columnsToDisplay = ['name']
-
-//   ngOnInit(): void {
-//      this.eventTypeForm = new FormGroup({
-//        name: new FormControl()
-//       });
-    
-//   }
-  
-//   displayedColumns: string = "Categorias";
-//   dataSource = CATEGORIES_DATA;
-
-  
-// }
  
 export interface Categories {
   name: string;
@@ -61,9 +24,6 @@ const ELEMENT_DATA: Categories[] = [
   styleUrls: ['./register-event-type.component.scss']
 })
 
-// this.eventTypeForm = new FormGroup({
-//   firstName: new FormControl()
-// });
 
 export class RegisterEventTypeComponent {
   columnsToDisplay: string[] = ['position', 'name'];
