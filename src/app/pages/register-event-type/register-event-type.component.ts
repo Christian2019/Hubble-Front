480751ea76228518d0fae569b0be53cc6c968279
Gustavo Jesinski Batista <gustavojesinskib@gmail.com>
2019-05-24 19:05:49 -0300

    tela renderizando, tabela aparecendo, nao conectado ao backend

--- src/app/pages/register-event-type/register-event-type.component.ts
+++ src/app/pages/register-event-type/register-event-type.component.ts
@@ -1,12 +1,57 @@
 import { Component, OnInit, NgModule } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
-export interface tabCategorias {
+// export interface tabCategorias {
+//   name: string;
+// }
+
+// const CATEGORIES_DATA: tabCategorias[] = [
+//   {name: 'Teste'},
+//   {name: 'teste2'},
+// ];
+
+
+// @Component({
+//   selector: 'app-register-event-type',
+//   templateUrl: './register-event-type.component.html',
+//   styleUrls: ['./register-event-type.component.scss']
+// })
+
+
+
+
+// export class RegisterEventTypeComponent implements OnInit {
+//   eventTypeForm: FormGroup;
+  
+//   columnsToDisplay = ['name']
+
+//   ngOnInit(): void {
+//      this.eventTypeForm = new FormGroup({
+//        name: new FormControl()
+//       });
+    
+//   }
+  
+//   displayedColumns: string = "Categorias";
+//   dataSource = CATEGORIES_DATA;
+
+  
+// }
+ 
+export interface Categories {
   name: string;
+  position: number;
+  
 }
 
-const CATEGORIES_DATA: tabCategorias[] = [
-  {name: 'Teste'},
-  {name: 'teste2'},
+const ELEMENT_DATA: Categories[] = [
+  {position: 1, name: 'categoria1'},
+  {position: 2, name: 'categoria2'},
+  {position: 3, name: 'categoria3'},
+  {position: 4, name: 'categoria4'},
+  {position: 5, name: 'categoria5'},
+  {position: 6, name: 'categoria6'},
+  {position: 7, name: 'categoria7'},
 ];
 
 
@@ -16,17 +61,11 @@ const CATEGORIES_DATA: tabCategorias[] = [
   styleUrls: ['./register-event-type.component.scss']
 })
 
+// this.eventTypeForm = new FormGroup({
+//   firstName: new FormControl()
+// });
 
-
-export class RegisterEventTypeComponent implements OnInit {
-  
-  ngOnInit(): void {
-    
-  }
-  
-  displayedColumns: string = "Categorias";
-  dataSource = CATEGORIES_DATA;
-
-  
+export class RegisterEventTypeComponent {
+  columnsToDisplay: string[] = ['position', 'name'];
+  dataSource = ELEMENT_DATA;
 }
-
